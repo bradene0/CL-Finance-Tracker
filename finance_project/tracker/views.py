a084a04f351596a76f3bb6afa2f43ce0f782fd4a
bradene0 <snave@thinkpad-thai.localdomain>
2025-08-10 18:27:23 -0500

    tracker url and views, html template

--- finance_project/tracker/views.py
+++ finance_project/tracker/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Transaction
+
 
 # Create your views here.
+def transaction_list(request):
+    all_transactions = Transaction.objects.all()
+    context = {
+    'transactions': all_transactions,
+    }
+    return render(request, 'tracker/transaction_list.html', context)
