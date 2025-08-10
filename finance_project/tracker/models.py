760e48b3ff347c99c44efa2baaa6f72b41d0646b
bradene0 <snave@thinkpad-thai.localdomain>
2025-08-10 17:25:58 -0500

    Models added

--- finance_project/tracker/models.py
+++ finance_project/tracker/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Transaction(models.Model):
+    date = models.DateField()
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.CharField(max_length=20)
+    description = models.TextField(help_text="Please enter a description of the transaction")
+    pass
