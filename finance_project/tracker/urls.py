df3dd3455de09c6b07c663101b837296e5093b23
bradene0 <snave@thinkpad-thai.localdomain>
2025-08-11 10:55:53 -0500

    ud in crud, need to add d path or modify edit path

--- finance_project/tracker/urls.py
+++ finance_project/tracker/urls.py
@@ -4,7 +4,9 @@ from . import views
 urlpatterns = [
 
     path('', views.transaction_list, name='transaction_list'),
-    path('add/', views.add_transaction, name='add_transaction')
+    path('add/', views.add_transaction, name='add_transaction'),
+    path('single_transact/<int:id>/', views.single_transact, name='single_transaction'),
+    path('edit_transact/<int:id>/', views.edit_transaction, name='edit_transaction')
 ]
 
 
--- finance_project/tracker/views.py
+++ finance_project/tracker/views.py
@@ -22,9 +22,35 @@ def add_transaction(request):
 
     return render(request, 'tracker/add_transaction.html', {'form': form})
 
+#---------------------------------------------------------
+def single_transact(request, id):
+    one_transaction = Transaction.objects.get(id=id)
+    context = {
+        'transaction': one_transaction,
+    }
 
+    return render(request, 'tracker/single_transact.html', context)
 
+#---------------------------------------------------------
+def edit_transaction(request, id):
+    transaction_to_edit = Transaction.objects.get(id=id)
 
+    if request.method == 'POST':
+        if 'delete' in request.POST:
+            transaction_to_edit.delete()
+            return redirect('transaction_list')
+        else:
+            form = TransactionForm(request.POST, instance=transaction_to_edit)
+        if form.is_valid():
+            form.save()
+            return redirect('transaction_list')
+    else:
+        form = TransactionForm(instance=transaction_to_edit)
+    
+    context = {
+        'form': form
+    }
+    return render(request, 'tracker/edit_transaction.html', context)
 
 
 
