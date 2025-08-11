aaabfe2f5f83fd2c1328ed7cce08f4f6dcec91d4
bradene0 <bradene0@outlook.com>
2025-08-11 17:55:19 -0500

    users started

--- finance_project/tracker/models.py
+++ finance_project/tracker/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Transaction(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     date = models.DateField()
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.CharField(max_length=20)
