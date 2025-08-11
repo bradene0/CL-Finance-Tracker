f62a4d32269c9bb8bd8c6ceab4fe628091ec4930
bradene0 <snave@thinkpad-thai.localdomain>
2025-08-10 21:35:14 -0500

    django forms

--- finance_project/tracker/views.py
+++ finance_project/tracker/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Transaction
-
+from .forms import TransactionForm
 
 # Create your views here.
 def transaction_list(request):
@@ -9,3 +9,30 @@ def transaction_list(request):
     'transactions': all_transactions,
     }
     return render(request, 'tracker/transaction_list.html', context)
+
+#--------------------------------------------------------
+def add_transaction(request):
+    if request.method == 'POST':
+       form = TransactionForm(request.POST)
+       if form.is_valid():
+            form.save()
+            return redirect('transaction_list')
+    else:
+        form = TransactionForm()
+
+    return render(request, 'tracker/add_transaction.html', {'form': form})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
